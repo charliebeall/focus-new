0e4093bed5f75440e0dee6bdcd8f7c15ffb60bdf
Scott Fairgrieve <scottfairgrieve@MacBook-Pro-4.local>
2016-10-23 22:00:18 -0400

    Updated publications, styling
    Former-commit-id: be7774e5c9fe73d963cbeebea3f39c4901ef5230 [formerly f7bfbfec8575c218369b2c6bf7025b39d5663028]
    Former-commit-id: c8af720f07071bef0a0a32c6161d28683ca56c4f

--- src/js/combined.js
+++ src/js/combined.js
@@ -13058,6 +13058,36 @@ Focus.Volumes = [
         number: 59,
         publications: [{
             type: 'Feature Article',
+            title: 'Living and Spirtual Worlds of Mali\'s Dogon People',
+            author: 'Thomas Wikle',
+            description: '',
+            thumbnail: 'publications/articles/mali/img/figure13.png',
+            url: 'publications/articles/mali/index.html',
+            location: [{
+                type: 'Feature',
+                geometry: {
+                    type: 'Point',
+                    coordinates: [-113.77222, 27.33072]
+                }
+            }]
+        }, {
+            type: 'Photo Essay',
+            title: 'Postcards from Oaxaca\'s Past and Present',
+            author: 'Scott Brady',
+            description: '',
+            thumbnail: 'publications/photoessays/oaxaca/img/1a.jpg',
+            url: 'publications/photoessays/oaxaca/index.html',
+            location: []
+        }, {
+            type: 'Geo Quiz',
+            title: 'Quiz Two:  Sustainability and Conservation',
+            author: 'Wesley Reisser',
+            description: '',
+            thumbnail: 'publications/quizzes/two/img/2050_logo.png',
+            url: 'publications/quizzes/two/index.html',
+            location: []
+        }, {
+            type: 'Feature Article',
             title: 'From Ranching to Fishing – the Cultural Landscape of the Northern Pacific Coast of Baja California, Mexico',
             author: 'Antoinette WinklerPrins, Pablo Alvarez, Gerardo Bocco, Ileana Espejel',
             description: '',
@@ -13091,36 +13121,6 @@ Focus.Volumes = [
             description: '',
             thumbnail: 'publications/quizzes/one/img/agsglobe.png',
             url: 'publications/quizzes/one/index.html',
-            location: []
-        }, {
-            type: 'Feature Article',
-            title: 'Living and Spirtual Worlds of Mali\'s Dogon People',
-            author: 'Thomas Wikle',
-            description: '',
-            thumbnail: 'publications/articles/mali/img/figure13.png',
-            url: 'publications/articles/mali/index.html',
-            location: [{
-                type: 'Feature',
-                geometry: {
-                    type: 'Point',
-                    coordinates: [-113.77222, 27.33072]
-                }
-            }]
-        }, {
-            type: 'Photo Essay',
-            title: 'Postcards from Oaxaca\'s Past and Present',
-            author: 'Scott Brady',
-            description: '',
-            thumbnail: 'publications/photoessays/oaxaca/img/1a.jpg',
-            url: 'publications/photoessays/oaxaca/index.html',
-            location: []
-        }, {
-            type: 'Geo Quiz',
-            title: 'Quiz Two:  Sustainability and Conservation',
-            author: 'Wesley Reisser',
-            description: '',
-            thumbnail: 'publications/quizzes/two/img/2050_logo.png',
-            url: 'publications/quizzes/two/index.html',
             location: []
         }]
     }
